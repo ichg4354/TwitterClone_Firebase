964d5e015f2a28eabb8819f0d1499c5852c85bc4
user <60804817+ichg4354@users.noreply.github.com>
2021-02-15 20:42:56 +0900

    create tweetPaint logic

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,17 +1,22 @@
 import Navigation from "components/Navigation";
 import { authService, dataService } from "fBase";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useEffect, useState } from "react/cjs/react.development";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
 
   const onSubmit = async (event) => {
+    const form = document.getElementById("tweetInput");
+    let text = form.value;
+    let DATE = Date.now();
     event.preventDefault();
     await dataService.collection("tweets").add({
       tweet: tweet,
-      date: Date.now(),
+      date: DATE,
     });
+    paintTweets(text, DATE);
     setTweet("");
   };
 
@@ -22,12 +27,32 @@ const Home = () => {
     setTweet(value);
   };
 
-  const getData = async () => {
-    const data = await dataService.collection("tweets").get({ query: "tweet" });
-    console.log(data);
+  const paintTweets = (text, id) => {
+    let tweetContainer = document.getElementById("tweetContainer");
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    li.id = id;
+    span.innerText = text;
+    li.appendChild(span);
+    tweetContainer.prepend(li);
   };
 
-  useEffect(() => getData(), []);
+  const getData = async () => {
+    let list = [];
+    const data = await dataService
+      .collection("tweets")
+      .orderBy("date", "desc")
+      .get();
+    data.forEach((each) => {
+      list.push({ id: each.data().date, text: each.data().tweet });
+    });
+    setTweets(list);
+  };
+
+  useLayoutEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <h1>HOME</h1>
@@ -37,10 +62,17 @@ const Home = () => {
           value={tweet}
           type="text"
           placeholder="Whats on your mind"
+          id="tweetInput"
         ></input>
         <input type="submit" value="Tweet!"></input>
       </form>
-      <ul></ul>
+      <ul id="tweetContainer">
+        {tweets.map((each) => (
+          <li key={each.id}>
+            <span>{each.text}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
